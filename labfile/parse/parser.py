ca4b0ae33bdccca262c9b69909fc5e7ef54a8a75
Rory Byrne <rory@rory.bio>
2024-11-05 22:10:37 +0000

    feat: add initial implementation of labfile parser
     - Introduced `.python-version` file specifying Python 3.12.4.
     - Added `labfile` package with modules for configuration, grammar, models, parsing, and transformation.
     - Implemented grammar in `labfile.lark` for parsing labfile syntax.
     - Created models for `Experiment`, `Provider`, and `Project` using Pydantic.
     - Developed a parser using Lark to convert labfile syntax into domain models.
     - Added tests for the parser with sample labfile syntax.
     - Configured project dependencies in `pyproject.toml` and lock files.

---
+++ labfile/parse/parser.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+
+from lark import Lark, Tree
+
+from labfile.model.project import Project
+from labfile.parse.transform import LabfileTransformer
+
+
+def _build_parser(grammar: Path) -> Lark:
+    assert grammar.exists(), f"Grammar not found: {grammar}"
+    labfile_grammar = grammar.read_text()
+
+    return Lark(labfile_grammar, start="start", parser="lalr")
+
+
+class Parser:
+    def __init__(self, grammar: Path, transformer: LabfileTransformer) -> None:
+        self._parser = _build_parser(grammar)
+        self._transformer = transformer
+
+    def parse(self, source: str) -> Project:
+        ast = self._parse_to_ast(source)
+        return self._parse_to_domain(ast)
+
+    ### PRIVATE #################################
+
+    def _parse_to_ast(self, source: str) -> Tree:
+        return self._parser.parse(source)
+
+    def _parse_to_domain(self, ast: Tree) -> Project:
+        return self._transformer.transform(ast)
---
+++ labfile/parse/transform.py
@@ -0,0 +1,124 @@
+from decimal import Decimal
+from lark import Transformer, Token
+import logging
+from typing import Any, TypeAlias, Union
+from pydantic import BaseModel
+from abc import ABC, abstractmethod
+
+from labfile.model.project import Experiment, Project, Provider
+
+logger = logging.getLogger(__name__)
+
+### INTERMEDIATE REPRESENTATION #################
+
+ParameterValue: TypeAlias = Union[int, float, str]
+
+
+class ExperimentName(BaseModel):
+    value: str
+
+    @classmethod
+    def from_token(cls, token: Token) -> "ExperimentName":
+        return cls(value=str(token))
+
+
+class Parameter(BaseModel):
+    name: str
+    value: ParameterValue
+
+    @classmethod
+    def create_hyperparameter(cls, name: str, value: ParameterValue) -> "Parameter":
+        return cls(name=name, value=value)
+
+
+class ParameterSet(BaseModel):
+    values: dict[str, ParameterValue]
+
+    @classmethod
+    def from_parameters(cls, parameters: list[Parameter]) -> "ParameterSet":
+        return cls(values={param.name: param.value for param in parameters})
+
+
+class ResourceDefinition(BaseModel, ABC):
+    name: str
+
+    @abstractmethod
+    def to_domain(self) -> BaseModel: ...
+
+
+class ExperimentDefinition(ResourceDefinition):
+    parameters: ParameterSet
+
+    def to_domain(self) -> Experiment:
+        return Experiment(name=self.name, parameters=self.parameters.values)
+
+
+class ProviderDefinition(ResourceDefinition):
+    def to_domain(self) -> Provider:
+        return Provider(name=self.name)
+
+
+### TRANSFORMER #################################
+
+
+class LabfileTransformer(Transformer):
+    """Convert an AST into a Domain object"""
+
+    def start(self, items: list[ResourceDefinition]) -> Project:
+        resources = [item.to_domain() for item in items]
+        experiments = [
+            resource for resource in resources if isinstance(resource, Experiment)
+        ]
+
+        providers = [
+            resource for resource in resources if isinstance(resource, Provider)
+        ]
+        return Project(experiments=experiments, providers=providers)
+
+    def statement(self, items: list[Any]) -> Any:
+        return items[0]
+
+    def provider(self, items: list[Union[Token, ParameterSet]]) -> ProviderDefinition:
+        provider_name = str(items[0])
+        return ProviderDefinition(name=provider_name)
+
+    def experiment(
+        self, items: list[Union[Token, ParameterSet]]
+    ) -> ExperimentDefinition:
+        experiment_name = str(items[1])
+        parameters = items[2]
+        if not isinstance(parameters, ParameterSet):
+            raise ValueError("Expected ParameterSet for experiment parameters")
+
+        return ExperimentDefinition(name=experiment_name, parameters=parameters)
+
+    def with_clause(self, items: list[Parameter]) -> ParameterSet:
+        return ParameterSet.from_parameters(items)
+
+    def with_param(self, items: list[Token]) -> Parameter:
+        return Parameter.create_hyperparameter(
+            name=str(items[0]), value=self._convert_value(items[1])
+        )
+
+    def value(self, items: list[Token]) -> Token:
+        return items[0]
+
+    def dotted_identifier(self, items: list[Token]) -> str:
+        return ".".join(str(item) for item in items)
+
+    def simple_identifier(self, items: list[Token]) -> str:
+        return str(items[0])
+
+    ### PRIVATE #################################
+
+    def _convert_value(self, token: Token) -> ParameterValue:
+        """Convert string values to appropriate numeric types"""
+        value = str(token)
+        is_numeric = value.replace(".", "", 1).isdigit()
+        if is_numeric:
+            if "." in value:
+                # Use Decimal for exact decimal representation
+                return float(Decimal(value))
+            return int(value)
+        else:
+            return value
